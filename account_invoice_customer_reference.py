6edddb023936af936677baa59beb1b23a2302d1f
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2014-01-27 14:24:23 +0200

    factoring agreement number to invoice reference prefix

--- account_invoice_customer_reference.py
+++ account_invoice_customer_reference.py
@@ -144,7 +144,7 @@ class account_invoice(osv.osv):
             except Exception, e:
                 print 'Cannot add company id to reference number.'
                 #logger.notifyChannel('bank_reference',netsvc.LOG_WARNING, 'Cannot add company id to reference number. %s - %s' % (inv.company_id,e))
-            maventaSettings = self.pool.get('account.maventa_settings').browse(cr, uid, 1, context=context)
+            maventaSettings = self.pool.get('account.maventa_settings').browse(cursor, user, 1, context=context)
             prefix = maventaSettings.factoring_contract_number + "".join(inv_no)
             myCompany = self.pool.get('res.users').browse(cursor, user, user).company_id
             #logger.notifyChannel('bank_reference',netsvc.LOG_DEBUG,'id: %s, inv_ref_type: %s' % (inv.id, myCompany.inv_ref_type))
